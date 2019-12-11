e07a5d237508954af70d245e86c6654ba686799b
Malte311 <malte.luttermann@student.uni-luebeck.de>
2019-12-11 11:46:38 +0100

    Improved logging

--- py-code/util.py
+++ py-code/util.py
@@ -1,11 +1,14 @@
+import datetime
 import os
 
 LOGFILE = 'log.txt'
 
 def log_exception(e):
+	ts = str(datetime.datetime.now())
+
 	if not os.path.isfile(LOGFILE):
 		with open(LOGFILE, 'w+') as file:
-			file.write(str(e) + '\r\n')
+			file.write(ts + '\r\n' + str(e) + '\r\n')
 	else:
 		with open(LOGFILE, 'a') as file:
-			file.write(str(e) + '\r\n')
+			file.write(ts + '\r\n' + str(e) + '\r\n')
